08cf33d7cd649f435cb3579f42b5b9432b92f9a5
mingyuchoo <mingyuchoo@gmail.com>
2022-04-26 20:40:00 +0900

    Update init.el

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -111,13 +111,13 @@
        ;;tty               ; improve the terminal Emacs experience
 
        :lang
-       agda              ; types of types of types of types...
-       beancount         ; mind the GAAP
+       ;;agda              ; types of types of types of types...
+       ;;beancount         ; mind the GAAP
        (cc +lsp)         ; C > C++ == 1
-       clojure           ; java with a lisp
+       ;;clojure           ; java with a lisp
        common-lisp       ; if you've seen one lisp, you've seen them all
-       coq               ; proofs-as-programs
-       crystal           ; ruby at the speed of c
+       ;;coq               ; proofs-as-programs
+       ;;crystal           ; ruby at the speed of c
        csharp            ; unity, .NET, and mono shenanigans
        data              ; config/data formats
        (dart +flutter)   ; paint ui and not much else
@@ -126,47 +126,47 @@
        elm               ; care for a cup of TEA?
        emacs-lisp        ; drown in parentheses
        erlang            ; an elegant language for a more civilized age
-       ess               ; emacs speaks statistics
-       factor
-       faust             ; dsp, but you get to keep your soul
-       fortran           ; in FORTRAN, GOD is REAL (unless declared INTEGER)
-       fsharp            ; ML stands for Microsoft's Language
-       fstar             ; (dependent) types and (monadic) effects and Z3
-       gdscript          ; the language you waited for
+       ;;ess               ; emacs speaks statistics
+       ;;factor
+       ;;faust             ; dsp, but you get to keep your soul
+       ;;fortran           ; in FORTRAN, GOD is REAL (unless declared INTEGER)
+       ;;fsharp            ; ML stands for Microsoft's Language
+       ;;fstar             ; (dependent) types and (monadic) effects and Z3
+       ;;gdscript          ; the language you waited for
        (go +lsp)         ; the hipster dialect
        (haskell +lsp)    ; a language that's lazier than I am
-       hy                ; readability of scheme w/ speed of python
-       idris             ; a language you can depend on
+       ;;hy                ; readability of scheme w/ speed of python
+       ;;idris             ; a language you can depend on
        json              ; At least it ain't XML
        (java +lsp)       ; the poster child for carpal tunnel syndrome
        javascript        ; all(hope(abandon(ye(who(enter(here))))))
-       julia             ; a better, faster MATLAB
+       ;;julia             ; a better, faster MATLAB
        kotlin            ; a better, slicker Java(Script)
        latex             ; writing papers in Emacs has never been so fun
-       lean              ; for folks with too much to prove
-       ledger            ; be audit you can be
-       lua               ; one-based indices? one-based indices
+       ;;lean              ; for folks with too much to prove
+       ;;ledger            ; be audit you can be
+       ;;lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
-       nim               ; python + lisp at the speed of c
+       ;;nim               ; python + lisp at the speed of c
        nix               ; I hereby declare "nix geht mehr!"
-       (ocaml +lsp)      ; an objective camel
-       org               ; organize your plain life in plain text
-       php               ; perl's insecure younger brother
-       plantuml          ; diagrams for confusing people more
+       ocaml             ; an objective camel
+       ;;org               ; organize your plain life in plain text
+       ;;php               ; perl's insecure younger brother
+       ;;plantuml          ; diagrams for confusing people more
        purescript        ; javascript, but functional
        python            ; beautiful is better than ugly
-       qt                ; the 'cutest' gui framework ever
-       racket            ; a DSL for DSLs
-       raku              ; the artist formerly known as perl6
-       rest              ; Emacs as a REST client
-       rst               ; ReST in peace
-       (ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
+       ;;qt                ; the 'cutest' gui framework ever
+       ;;racket            ; a DSL for DSLs
+       ;;raku              ; the artist formerly known as perl6
+       ;;rest              ; Emacs as a REST client
+       ;;rst               ; ReST in peace
+       ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        scala             ; java, but good
        (scheme +guile)   ; a fully conniving family of lisps
        sh                ; she sells {ba,z,fi}sh shells on the C xor
-       sml
-       solidity          ; do you need a blockchain? No.
+       ;;sml
+       ;;solidity          ; do you need a blockchain? No.
        swift             ; who asked for emoji variables?
        terra             ; Earth and Moon in alignment for performance.
        web               ; the tubes
