1a6dedd6dd39797851345e91374c0bb7cd740fb7
mingyuchoo <mingyuchoo@gmail.com>
2022-09-04 06:40:25 +0900

    Update packages.el

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -49,6 +49,6 @@
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;(unpin! t)
 
-;;(package! haskell-mode)
-;;(package! lsp-haskell)
+(package! haskell-mode)
+(package! lsp-haskell)
 
