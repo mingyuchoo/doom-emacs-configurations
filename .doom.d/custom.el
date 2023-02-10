bb88f7a7666ee65297305e3c479b6db418bb9c52
mingyuchoo <mingyuchoo@gmail.com>
2023-02-11 06:10:39 +0900

    Update custom.el

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -10,3 +10,5 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(with-eval-after-load "lsp-mode"
+    (setq lsp-restart 'ignore))
