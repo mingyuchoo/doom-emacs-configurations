09bd9f61fe4347b6c11718c9f40b14b39a04f3be
mingyuchoo <mingyuchoo@gmail.com>
2024-08-27 07:54:55 +0900

    updated

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -136,6 +136,15 @@
         '((sequence "TODO" "DOING" "BLOCKED" "DONE"))))
 
 
+;; Encoding
+(progn
+  (set-language-environment "UTF-8")
+  (set-default-coding-systems 'utf-8)
+  (set-terminal-coding-system 'utf-8)
+  (set-keyboard-coding-system 'utf-8)
+  (prefer-coding-system 'utf-8))
+
+
 ;; Eshell
 (set-eshell-alias! "ll" "ls -l $*"
                    "la" "ls -la $*"
